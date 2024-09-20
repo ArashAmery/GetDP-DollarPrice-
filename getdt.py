7161582fda2f7ac6d3200125ffc77034427eb491
ArashAmery <arash411485@gmail.com>
2024-09-20 12:08:59 -0700

    connect to website with requests module

--- getdt.py
+++ getdt.py
@@ -1,2 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+request = requests.get("https://donya-e-eqtesad.com/tags/%D9%82%DB%8C%D9%85%D8%AA_%D8%AF%D9%84%D8%A7%D8%B1")
+if(request.status_code == 200):
+    pass
+else:
+    print("can't conection")
